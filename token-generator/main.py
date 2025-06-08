e948b97946e316682a283b7886411c6159d94237
mfedorchuk <maksym.fedorchuk@corp.kismia.com>
2025-06-08 14:37:16 +0200

    dev: add heaven_data_token endpoint for token generation.

---
+++ token-generator/main.py
@@ -0,0 +1,32 @@
+import hashlib
+from datetime import datetime, timedelta, timezone
+
+from loguru import logger
+import functions_framework
+
+from google.cloud import bigquery
+
+TOKEN_TABLE_PATH = "publicdatasource.heavens_data.tokens"
+
+
+def generate_token_row(email: str) -> dict[str, str]:
+    return {
+        "email": email,
+        "token": hashlib.shake_128(email.encode()).hexdigest(16),
+        "valid_from": datetime.now(timezone.utc),
+        "valid_to": datetime.now(timezone.utc) + timedelta(days=365)
+    }
+
+
+@functions_framework.http
+def heaven_data_token(request):
+    user_email = request.headers.get('Email')
+
+    client = bigquery.Client()
+    new_token_row = generate_token_row(user_email)
+
+    insert_errors = client.insert_rows_json(table=TOKEN_TABLE_PATH, json_rows=[new_token_row])
+    if insert_errors:
+        logger.error(f"Errors while inserting new token: {insert_errors}")
+    else:
+        logger.success(f"Rows successfully inserted for {user_email}")
